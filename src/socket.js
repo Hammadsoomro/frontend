1c5c578465f6dadb22286f68d82b5bbb68633ef0
Hammadsoomro <165611609+Hammadsoomro@users.noreply.github.com>
2025-06-26 12:31:17 +0500

    socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Remove extra space from URL!
-const SOCKET_URL = "http://localhost:8080"; // Change to your backend URL in production
+const SOCKET_URL = "https://backend-connectify.up.railway.app/api"; // Change to your backend URL in production
 
 const socket = io(SOCKET_URL, { transports: ["websocket"] });
 export default socket;
